7497d6ad976211ece65ab46d354f2cca00342cc9
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2022-01-20 16:49:27 -0300

    feat: Entity Person created

---
+++ src/main/java/org/dio/desafio/entities/Person.java
@@ -0,0 +1,40 @@
+package org.dio.desafio.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Entity
+@Table(name = "Persons")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstname;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    @Column(nullable = false)
+    private String cpf;
+
+    private LocalDate birthDate;
+
+    @OneToMany(fetch = FetchType.LAZY,
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    private List<Phone> phones;
+
+    @Column(nullable = false)
+    private Boolean isActive;
+
+}
