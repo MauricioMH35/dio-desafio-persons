452b40a219fef858e88c6d0f2c9e356710c4efce
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2022-01-20 18:09:10 -0300

    Finish Person

--- src/main/java/org/dio/desafio/services/PersonService.java
+++ src/main/java/org/dio/desafio/services/PersonService.java
@@ -1,7 +1,9 @@
 package org.dio.desafio.services;
 
+import org.dio.desafio.dto.mappers.PersonMapper;
 import org.dio.desafio.dto.requests.PersonDTO;
 import org.dio.desafio.dto.responses.ResponseDTO;
+import org.dio.desafio.entities.Person;
 import org.dio.desafio.exceptions.PersonConflictException;
 import org.dio.desafio.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,18 +14,25 @@ public class PersonService {
 
     private final PersonRepository repository;
 
+    private final PersonMapper mapper;
+
     @Autowired
-    public PersonService(PersonRepository repository) {
+    public PersonService(PersonRepository repository, PersonMapper mapper) {
         this.repository = repository;
+        this.mapper = mapper;
     }
 
     public ResponseDTO save(PersonDTO personDTO) throws PersonConflictException {
         if(!repository.findByCpf(personDTO.getCpf()).equals(null))
             throw new PersonConflictException();
 
-        repository.save(personDTO);
+        Person saved = mapper.toEntity(personDTO);
+        saved = repository.save(saved);
 
-        ResponseDTO response = createGenericResponseMessage("Person successfully created with \'id\' ", );
+        ResponseDTO response = createResponseMessage(
+                "Person successfully created with \'id\' ",
+                saved.getId().toString());
+
         return null;
     }
 
