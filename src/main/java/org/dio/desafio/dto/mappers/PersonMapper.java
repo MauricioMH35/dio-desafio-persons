73f174f718999d4182ae7b8bf27c9fc33d367fb5
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2022-01-20 19:44:57 -0300

    feat: Brith Date Treatment

--- src/main/java/org/dio/desafio/dto/mappers/PersonMapper.java
+++ src/main/java/org/dio/desafio/dto/mappers/PersonMapper.java
@@ -3,10 +3,13 @@ package org.dio.desafio.dto.mappers;
 import org.dio.desafio.dto.requests.PersonDTO;
 import org.dio.desafio.entities.Person;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.NullValueCheckStrategy;
 
 @Mapper(componentModel = "spring")
 public interface PersonMapper {
 
+    @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
     Person toEntity(PersonDTO personDTO);
 
     PersonDTO toDTO(Person person);
