5d1a6efb28accc6724590d513e6714e7a60c2f18
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2022-01-20 17:05:31 -0300

    feat: DTO Person implemented with the fields

---
+++ src/main/java/org/dio/desafio/dto/requests/PersonDTO.java
@@ -0,0 +1,40 @@
+package org.dio.desafio.dto.requests;
+
+import lombok.*;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Builder
+public class PersonDTO {
+
+    private Long id;
+
+    @NotNull
+    @Size(min = 2, max = 100)
+    private String firstname;
+
+    @NotNull
+    @Size(min = 2, max = 100)
+    private String lastname;
+
+    @NotNull
+    @CPF
+    private String cpf;
+
+    private String birthDate;
+
+    @Valid
+    @NotNull
+    private List<PhoneDTO> phones;
+
+    private Boolean isActive;
+
+}
