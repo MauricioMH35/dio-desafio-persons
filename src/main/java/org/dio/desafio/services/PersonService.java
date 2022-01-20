a8f7422353e840aa6118041fa4c3096050c0c347
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2022-01-20 17:16:28 -0300

    Finish the DTO Person

--- src/main/java/org/dio/desafio/services/PersonService.java
+++ src/main/java/org/dio/desafio/services/PersonService.java
@@ -1,7 +1,21 @@
 package org.dio.desafio.services;
 
+import org.dio.desafio.dto.requests.PersonDTO;
+import org.dio.desafio.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Service
 public class PersonService {
 
+    private final PersonRepository repository;
+
+    @Autowired
+    public PersonService(PersonRepository repository) {
+        this.repository = repository;
+    }
+
+    public String save(PersonDTO personDTO) {
+
+    }
 }
